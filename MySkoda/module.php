00240f87ca4b52167af94b27c22e896a54f7e1b2
taloriko <67206692+taloriko@users.noreply.github.com>
2026-09-16 22:17:42 +0200

    Wire VIN decoding into MySkoda 1.3

--- MySkoda/module.php
+++ MySkoda/module.php
@@ -13,6 +13,8 @@ require_once __DIR__ . '/src/NotificationTrait.php';
 require_once __DIR__ . '/src/HelpersTrait.php';
 require_once __DIR__ . '/src/CommandTrait.php';
 require_once __DIR__ . '/src/ClimateSelectionTrait.php';
+require_once __DIR__ . '/src/VinDecoderTrait.php';
+require_once __DIR__ . '/src/VinIntegrationTrait.php';
 
 final class MySkoda extends IPSModuleStrict
 {
@@ -26,9 +28,12 @@ final class MySkoda extends IPSModuleStrict
         MySkodaNotificationTrait,
         MySkodaHelpersTrait,
         MySkodaCommandTrait,
-        MySkodaClimateSelectionTrait {
+        MySkodaClimateSelectionTrait,
+        MySkodaVinDecoderTrait,
+        MySkodaVinIntegrationTrait {
         MySkodaCoreTrait::Create as private coreCreate;
         MySkodaCoreTrait::ApplyChanges as private coreApplyChanges;
+        MySkodaCoreTrait::GetConfigurationForm as private coreGetConfigurationForm;
         MySkodaVariablesTrait::registerVariables as private baseRegisterVariables;
         MySkodaVariablesTrait::setPathValue as private baseSetPathValue;
 
@@ -36,8 +41,13 @@ final class MySkoda extends IPSModuleStrict
         MySkodaCommandTrait::ApplyChanges as private commandApplyChanges;
         MySkodaCommandTrait::RequestAction as private commandRequestAction;
 
-        MySkodaClimateSelectionTrait::Create insteadof MySkodaCoreTrait, MySkodaCommandTrait;
-        MySkodaClimateSelectionTrait::ApplyChanges insteadof MySkodaCoreTrait, MySkodaCommandTrait;
+        MySkodaClimateSelectionTrait::Create as private climateCreate;
+        MySkodaClimateSelectionTrait::ApplyChanges as private climateApplyChanges;
+
+        MySkodaVinIntegrationTrait::Create insteadof MySkodaCoreTrait, MySkodaCommandTrait, MySkodaClimateSelectionTrait;
+        MySkodaVinIntegrationTrait::ApplyChanges insteadof MySkodaCoreTrait, MySkodaCommandTrait, MySkodaClimateSelectionTrait;
+        MySkodaVinIntegrationTrait::GetConfigurationForm insteadof MySkodaCoreTrait;
+
         MySkodaClimateSelectionTrait::RequestAction insteadof MySkodaCoreTrait, MySkodaCommandTrait;
         MySkodaClimateSelectionTrait::setPathValue insteadof MySkodaVariablesTrait;
 
@@ -53,7 +63,7 @@ final class MySkoda extends IPSModuleStrict
 
     private const API_ROOT = 'https://public.api.connect.skoda-auto.cz';
     private const OPENAPI_URL = self::API_ROOT . '/v3/api-docs';
-    private const USER_AGENT = 'Symcon-MySkoda/1.2';
+    private const USER_AGENT = 'Symcon-MySkoda/1.3';
     private const QUOTA_RESERVE = 2;
 
     public function Update(): void
